c734f470b0479c7c9331f631c45df234d5640eb1
Anna <annadalex@icloud.com>
2024-01-24 20:05:22 +0100

    Terminato

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
+const btnHtml = document.getElementById("btnPlay")
+
+const grigliaHtml = document.getElementById("griglia")
 
 
-const grigliaHtml = document.getElementById(`griglia`)
-for(let i = 1; i<100; i++ ){
+
+
+btnHtml.addEventListener(`click`, function(){
+
+  grigliaHtml.style.setProperty("display","flex")
+
+  for(let i = 1; i<100; i++ ){
     let box= document.createElement("div")
     box.classList.add("box")
+    box.innerHTML=`<span>${[i]}</span>`
+
+    box.addEventListener(`click`, function(){
+        let scegliSpan= this.querySelector("span").innerText
+
+        this.classList.toggle("sky")
+    })  
+
+
     grigliaHtml.append(box)
 }
+})
+
